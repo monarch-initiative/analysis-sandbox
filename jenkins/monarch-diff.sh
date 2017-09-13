2b8c623eae22348fde763d5bf084d5032d91c9fd
kshefchek <kshefchek@gmail.com>
2017-09-12 17:44:58 -0700

    fix ontology endpoint

---
+++ jenkins/monarch-diff.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+REMOTE_SERVER="monarch-ttl-prod"
+export timestamp=$(date +%Y%m%d%H%M)
+directory=data-diff-"${timestamp}"
+mkdir $directory
+cd data-diff-"${timestamp}"
+
+virtualenv -p /usr/bin/python3 venv
+source venv/bin/activate
+pip install requests markdown
+
+python3 ./monarch/monarch-data-diff.py --config ./conf/monarch-qc.json --threshold 10
+deactivate
+
+grep SEVERE /var/lib/jenkins/jobs/load-scigraph-data-on-dev/lastSuccessful/log | perl -e '$pos; while(<>){chomp; if ($_ =~ m/.*clique.*/){ $pos = 1;} elsif ($pos == 1){ $_ =~ s/SEVERE: //; print "\n$_"; $pos=2;} elsif($pos == 2){ $_ =~ s/SEVERE: //; print "\t$_";}}' | sed '/^$/d' > clique-warnings.tsv
+
+
+scp -r ./$directory monarch@$REMOTE_SERVER:/var/www/data/qc/
--- monarch/monarch-data-diff.py
+++ monarch/monarch-data-diff.py
@@ -3,6 +3,8 @@
 
 Generate a diff of solr facet counts
 and scigraph queries as markdown and html
+
+TODO support lists of facets
 """
 import requests
 from requests import Request, Session
@@ -10,14 +12,17 @@ import argparse
 from pathlib import Path
 import logging
 import json
-from typing import List, Tuple, Iterable
+from typing import List, Tuple, Iterable, Dict, Union, Any, Set
 import copy
 import itertools
 import re
 import markdown
 
+
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+JSONType = Union[str, int, float, bool, None, Dict[str, Any], List[Any]]
 
 
 def main():
@@ -25,8 +30,10 @@ def main():
     parser = argparse.ArgumentParser(usage=__doc__)
     parser.add_argument('--config', '-c', required=True, help='JSON configuration file')
     parser.add_argument('--out', '-o', required=False, help='output directory', default="./")
+    parser.add_argument('--threshold', '-t', required=False, help='diff threshold', default=10, type=int)
     args = parser.parse_args()
     dir_path = Path(args.out)
+    threshold = float(args.threshold/100)
 
     md_path = dir_path / 'monarch-diff.md'
     md_file = md_path.open("w")
@@ -44,6 +51,12 @@ def main():
         'facet.mincount': '1',
         'facet.sort': 'count',
         'indent': 'on'
+    }
+
+    golr_default_params = {
+        'q': '*:*',
+        'wt': 'json',
+        'fl': 'subject,object'
     }
 
     conf_fh = open(args.config, 'r')
@@ -65,6 +78,42 @@ def main():
         prod_results = get_facets(solr_prod, golr_facet_params)
         dev_results = get_facets(solr_dev, golr_facet_params)
         diff = diff_facets(dev_results, prod_results)
+        formatted_diff = convert_diff_to_md(diff)
+
+        # Create a filtered object based on the threshold passed in (or 10%)
+        # If there is a x% decrease in data pass this on to generate a data
+        # diff
+        filtered_diff = {k:v for k,v in diff.items()
+                         if (v[0] == 0 and v[1] < 0) or
+                         (v[1] < 0 and 1 - v[0] / (v[0] + abs(v[1])) >= threshold)}
+
+        for dropped_data in filtered_diff:
+            if dropped_data == 'Total' or \
+                   dropped_data.startswith(":.well-known"):
+                continue
+
+            diff_dir = dir_path / q_name.replace(' ', '_').lower()
+            diff_dir.mkdir(parents=True, exist_ok=True)
+
+            diff_path = diff_dir / "{}.tsv".format(dropped_data.replace(' ', '_').lower())
+            diff_file = diff_path.open("w")
+
+            params = copy.deepcopy(golr_default_params)
+            params['fq'] = copy.deepcopy(query['filters'])
+            if dropped_data == 'Other':
+                params['fq'].append('-{}:[* TO *]'.format(query['facet.field'], dropped_data))
+            else:
+                params['fq'].append('{}:"{}"'.format(query['facet.field'], dropped_data))
+            logger.info("Processing data diff for params {}".format(params['fq']))
+
+            query_pairs = get_solr_so_pairs(solr_dev, params)
+            reference_pairs = get_solr_so_pairs(solr_prod, params)
+
+            solr_diff = diff_solr_so_data(query_pairs, reference_pairs)
+            for sub, objects in solr_diff.items():
+                for obj in objects:
+                    diff_file.write("{}\t{}\n".format(sub, obj))
+            diff_file.close()
 
         md_file.write("{}\n".format(add_md_header(q_name, 4)))
         sesh = Session()
@@ -76,7 +125,7 @@ def main():
         md_file.write(add_href(dev_req.url, "Dev Query"))
         md_file.write('\n\n')
 
-        diff_list = [(k, v) for k, v in diff.items()]
+        diff_list = [(k, v) for k, v in formatted_diff.items()]
         diff_list.sort(key=lambda tup: int(re.search(r'\d+', tup[1]).group(0)), reverse=True)
         md_file.write(add_md_table(diff_list, query['headers']))
         md_file.write("\n\n")
@@ -105,6 +154,7 @@ def get_scigraph_diff(scigraph_prod: str, scigraph_dev: str,
     prod_results = get_scigraph_results(scigraph_prod, conf['query'])
     dev_results = get_scigraph_results(scigraph_dev, conf['query'])
     diff = diff_facets(dev_results, prod_results)
+    formatted_diff = convert_diff_to_md(diff)
 
     output_md += "{}\n".format(add_md_header(query_name, 4))
 
@@ -121,7 +171,7 @@ def get_scigraph_diff(scigraph_prod: str, scigraph_dev: str,
     output_md += add_href(dev_req.url, "Dev Query")
     output_md += '\n\n'
 
-    diff_list = [(k, v) for k, v in diff.items()]
+    diff_list = [(k, v) for k, v in formatted_diff.items()]
     diff_list.sort(key=lambda tup: int(re.search(r'\d+', tup[1]).group(0)), reverse=True)
     output_md += add_md_table(diff_list, conf['headers'])
     output_md += "\n\n"
@@ -167,21 +217,22 @@ def add_href(link: str, display_name: str) -> str:
     return "[{}]({})".format(display_name, link)
 
 
-def get_facets(solr_server: str, params:dict) -> dict:
+def get_facets(solr_server: str,
+               params: Dict[str, Union[str, List[str]]]) -> Dict[str, int]:
     solr_request = requests.get(solr_server, params=params)
     response = solr_request.json()
     facet = params['facet.field']
     result_count = response['response']['numFound']
     facet_result = response['facet_counts']['facet_fields'][facet]
     facet_obj = dict(itertools.zip_longest(*[iter(facet_result)] * 2, fillvalue=""))
-    res_sum = sum([v for k,v in facet_obj.items()])
+    res_sum = sum([v for k, v in facet_obj.items()])
     other_count = result_count - res_sum
     facet_obj['Total'] = result_count
     facet_obj['Other'] = other_count
     return facet_obj
 
 
-def get_scigraph_results(scigraph_server: str, query:dict) -> dict:
+def get_scigraph_results(scigraph_server: str, query: str) -> JSONType:
     params = {
         'cypherQuery': query
     }
@@ -191,7 +242,7 @@ def get_scigraph_results(scigraph_server: str, query:dict) -> dict:
     return results
 
 
-def diff_facets(query: dict, reference: dict) -> dict:
+def diff_facets(query: Dict, reference: Dict) -> Dict[str, List[int]]:
     diff_obj = copy.deepcopy(query)
     for key in reference:
         if key not in query:
@@ -200,14 +251,60 @@ def diff_facets(query: dict, reference: dict) -> dict:
         if key not in reference:
             reference[key] = 0
         diff = query[key] - reference[key]
-        if diff == 0:
-            diff_obj[key] = "{} ({})".format(query[key], diff)
-        elif diff > 0:
-            diff_obj[key] = "{} (+{})".format(query[key], add_bold(diff))
-        else:
-            diff_obj[key] = "{} (-{})".format(query[key], add_italics(abs(diff)))
+        diff_obj[key] = [query[key], diff]
 
     return diff_obj
+
+
+def convert_diff_to_md(diff: Dict[str, List[int]]) -> Dict[str, str]:
+    diff_obj = copy.deepcopy(diff)
+    for k, v in diff.items():
+        count, diff = v
+        if diff == 0:
+            diff_obj[k] = "{} ({})".format(count, diff)
+        elif diff > 0:
+            diff_obj[k] = "{} (+{})".format(count, add_bold(diff))
+        else:
+            diff_obj[k] = "{} (-{})".format(count, add_italics(abs(diff)))
+    return diff_obj
+
+
+def diff_solr_so_data(query: Dict[str, Set[str]],
+                      reference: Dict[str, Set[str]]) -> Dict[str, Set[str]]:
+    results = {}
+    for sub in reference:
+        results[sub] = set()
+        try:
+            results[sub] = reference[sub] - query[sub]
+        except KeyError:
+            results[sub] = reference[sub]
+
+    return results
+
+
+def get_solr_so_pairs(
+        solr_server: str,
+        params: Dict[str, Union[str, List[str]]]) -> Dict[str, Set[str]]:
+    results = {}
+    params['start'] = 0
+    params['rows'] = 1000
+
+    result_count = params['rows']
+
+    while params['start'] < result_count:
+        solr_request = requests.get(solr_server, params=params)
+        response = solr_request.json()
+        result_count = response['response']['numFound']
+        if result_count == 0:
+            break
+        for doc in response['response']['docs']:
+            try:
+                results[doc['subject']].add(doc['object'])
+            except KeyError:
+                results[doc['subject']] = {doc['object']}
+
+        params['start'] += params['rows']
+    return results
 
 
 if __name__ == "__main__":
