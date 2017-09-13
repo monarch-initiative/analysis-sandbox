fac380d01fd776810e7ead3c4c68f2e813a56ad0
kshefchek <kshefchek@gmail.com>
2017-09-12 17:52:50 -0700

    fix rel path

--- jenkins/monarch-diff.sh
+++ jenkins/monarch-diff.sh
@@ -12,7 +12,7 @@ virtualenv -p /usr/bin/python3 venv
 source venv/bin/activate
 pip install requests markdown
 
-python3 ../../monarch/monarch-data-diff.py --config ../../conf/monarch-qc.json --threshold 10
+python3 ../monarch/monarch-data-diff.py --config ../conf/monarch-qc.json --threshold 10
 deactivate
 
 grep SEVERE /var/lib/jenkins/jobs/load-scigraph-data-on-dev/lastSuccessful/log | perl -e '$pos; while(<>){chomp; if ($_ =~ m/.*clique.*/){ $pos = 1;} elsif ($pos == 1){ $_ =~ s/SEVERE: //; print "\n$_"; $pos=2;} elsif($pos == 2){ $_ =~ s/SEVERE: //; print "\t$_";}}' | sed '/^$/d' > clique-warnings.tsv
