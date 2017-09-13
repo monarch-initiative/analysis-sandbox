7319c5d974d5834473f8e389471c11fc74e2b496
kshefchek <kshefchek@gmail.com>
2017-09-12 20:12:17 -0700

    forgot clique file

--- jenkins/monarch-diff.sh
+++ jenkins/monarch-diff.sh
@@ -11,10 +11,10 @@ virtualenv -p /usr/bin/python3 venv
 source venv/bin/activate
 pip install requests markdown
 
-python3 ../monarch/monarch-data-diff.py --config ../conf/monarch-qc.json --threshold 10 --out ./data-diff-"${timestamp}"
+python3 ./monarch/monarch-data-diff.py --config ./conf/monarch-qc.json --threshold 10 --out ./data-diff-"${timestamp}"
 deactivate
 
-grep SEVERE /var/lib/jenkins/jobs/load-scigraph-data-on-dev/lastSuccessful/log | perl -e '$pos; while(<>){chomp; if ($_ =~ m/.*clique.*/){ $pos = 1;} elsif ($pos == 1){ $_ =~ s/SEVERE: //; print "\n$_"; $pos=2;} elsif($pos == 2){ $_ =~ s/SEVERE: //; print "\t$_";}}' | sed '/^$/d' > clique-warnings.tsv
+grep SEVERE /var/lib/jenkins/jobs/load-scigraph-data-on-dev/lastSuccessful/log | perl -e '$pos; while(<>){chomp; if ($_ =~ m/.*clique.*/){ $pos = 1;} elsif ($pos == 1){ $_ =~ s/SEVERE: //; print "\n$_"; $pos=2;} elsif($pos == 2){ $_ =~ s/SEVERE: //; print "\t$_";}}' | sed '/^$/d' > ./data-diff-"${timestamp}"/clique-warnings.tsv
 
 
 scp -r ./$directory monarch@$REMOTE_SERVER:/var/www/data/qc/
