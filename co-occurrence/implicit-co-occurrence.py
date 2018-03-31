d7984b95f8bbb44a4edc09bea7b6ff5e1a846054
kshefchek <kshefchek@gmail.com>
2018-03-30 18:05:06 -0700

    update implicit co-occurrence script

--- co-occurrence/implicit-co-occurrence.py
+++ co-occurrence/implicit-co-occurrence.py
@@ -2,10 +2,10 @@ import itertools
 import requests
 from collections import Counter
 
+
 solr = "https://solr-dev.monarchinitiative.org/solr/golr/select/"
 
-closure_list = []
-
+closure_map = {}
 
 def get_solr_docs(solr, params):
     resultCount = params['rows']
@@ -22,7 +22,7 @@ params = {
     'rows': 500,
     'start': 0,
     'q': '*:*',
-    'fl': 'object_closure_label',
+    'fl': 'subject,object_closure',
     'fq': [
         'subject_category:"disease"',
         'object_category:"phenotype"',
@@ -31,16 +31,67 @@ params = {
 }
 
 for doc in get_solr_docs(solr, params):
-    closure_list.append(doc['object_closure_label'])
+    try:
+        closures = [pheno[3:] for pheno in doc['object_closure'] if pheno.startswith("HP:")]
+        closure_map[doc['subject']].extend(closures)
+    except KeyError:
+        closures = [pheno[3:] for pheno in doc['object_closure'] if pheno.startswith("HP:")]
+        closure_map[doc['subject']] = closures
 
-# Credit https://stackoverflow.com/a/49079618
+closure_list = [closures for closures in closure_map.values()]
+closure_map = None
+
+# Get co-occurring terms in a list of lists, credit
+# https://stackoverflow.com/a/42272583
 
 # generate combinations for each sub list seperately
 lists_of_pairs = [list(itertools.combinations(sub_list, 2)) for sub_list in closure_list]
+closure_list = None
 # flatten the lists of pairs to 1 large list of pairs
-all_pairs = [pair for pairs_list in lists_of_pairs for pair in pairs_list]
+all_pairs = [pair if pair[0] >= pair[1] else pair[::-1] for pairs_list in lists_of_pairs for pair in pairs_list]
+lists_of_pairs = None
+
 # let the Counter do the rest for you
 co_occurrence_counts = Counter(all_pairs)
 
+# Get a id-label map
+solr_search = "https://solr-dev.monarchinitiative.org/solr/search/select/"
+
+params = {
+    'wt': 'json',
+    'rows': 500,
+    'start': 0,
+    'q': '*:*',
+    'fl': 'id,label',
+    'fq': [
+        'prefix:HP',
+        'category:Phenotype',
+     ]
+}
+
+label_map = dict()
+
+for doc in get_solr_docs(solr_search, params):
+   label_map[doc['id']] = doc['label'][0]
+   try:
+       for curie in doc['equivalent_curie']:
+           if curie.startswith('HP'):
+               label_map[curie] = doc['label'][0]
+   except KeyError:
+       pass
+
+label_map['HP:0000001'] = 'All'
+
 for k,v in co_occurrence_counts.items():
-    print("{}\t{}\t{}".format(k[0], k[1], v))
+    curie1 = "HP:{}".format(k[0])
+    curie2 = "HP:{}".format(k[1])
+    try:
+        label1 = label_map[curie1]
+    except KeyError:
+        label1 = ''
+    try:
+        label2 = label_map[curie2]
+    except KeyError:
+        label2 = ''
+    print("{}\t{}\t{}\t{}\t{}".format(curie1, label1, curie2, label2, v))
+
